ccc5ccfb9457a74e7d4a4f2be587c4fa2612c1b8
carlo <carlozecca1@gmail.com>
2026-01-20 16:26:56 +0100

    change the NOTICE

--- tdd_agent/config.py
+++ tdd_agent/config.py
@@ -1,3 +1,25 @@
+#
+# Copyright (c) 2025 
+# Prof. Domenico Amalfitano and Antonio Giaquinto University of Naples Federico II
+# Prof. Filippo Ricca and Carlo Arturo Zecca University of Genoa
+#
+# This file is part of the GenAITestAgent project.
+# Developed as part of a bachelor's theses in Computer Engineering and Computer Science
+# under the supervision of Prof. Domenico Amalfitano and Prof. Filippo Ricca
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.resolve()
